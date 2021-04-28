75aa4ab1aa03fc3b9692303af502a66566812fcc
johnc449 <71931044+johnc449@users.noreply.github.com>
2021-04-28 18:12:54 +0100

    done my bit

--- src/busInfo/busInfo/tripSearch.java
+++ src/busInfo/busInfo/tripSearch.java
@@ -1,18 +1,31 @@
+package busInfo;
+import java.io.File;
 import java.util.ArrayList;
-package tripSearch.java
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+
+//Takes search term in string HH:MM:SS format
+//Returns arrayList full of arrays
+// So arrayList.get(0) would contain an array with first line data
+// So arrayList.get(0)[0] would contain first field in first line#
+// Also checks lines in file for errors
 public class tripSearch{
-	public static void search(String searchTerm) {
-        try{
-	    ArrayList<String[]> tripDetails = new ArrayList<>(0);
-        File map = new File("stop_times.txt");
+	public static ArrayList<String[]> search(String searchTerm) {
+		ArrayList<String[]> tripDetails = new ArrayList<>(0);
+		try {
+        File map = new File("busInfo/stop_times.txt");
 	    Scanner myReader = new Scanner(map);
     	myReader.nextLine();
         while (myReader.hasNextLine()) {
-          String[] currentLine = myReader.nextLine.split(",");
+          String[] currentLine = myReader.nextLine().split(",");
           if(currentLine.length>2){
-          if(currentLine[1].replace(" ","0").equals(searchTerm)){
-              tripDetails.add({currentLine[0],currentLine[1].replace(" ","0"),currentLine[2].replace(" ","0"),currentLine[3]
-              ,currentLine[4],currentLine[5],currentLine[6],currentLine[7],currentLine[8]})
+          if(currentLine[1].replace(" ","0").equals(searchTerm) &&
+             tripSearch.timeCheck(currentLine[1].replace(" ","0"),currentLine[2].replace(" ","0"))==true
+          ){
+        	  String[] tempArray = {currentLine[0],currentLine[1].replace(" ","0"),currentLine[2].replace(" ","0"),currentLine[3]
+                      ,currentLine[4],currentLine[5],currentLine[6],currentLine[7]};
+              tripDetails.add(tempArray);
           }}
         }
         myReader.close();  } catch (FileNotFoundException ex){
@@ -20,5 +33,43 @@ public class tripSearch{
         }
         return tripDetails;
 }
+    private static Boolean timeCheck(String startTime, String endTime){
+        String[] startTimeSplit = startTime.split(":");
+        if(startTimeSplit.length != 3){
+            return false;
+        }
+        int hour = Integer.parseInt(startTimeSplit[0]);
+        if(hour>24 || hour<0){
+            return false;
+        }
+        int minute = Integer.parseInt(startTimeSplit[1]);
+        if(minute < 0 || minute > 60){
+            return false;
+        }
+        int second = Integer.parseInt(startTimeSplit[2]);
+        if(second < 0 || second >60){
+            return false;
+        }
+
+        String[] endTimeSplit = endTime.split(":");
+        if(endTimeSplit.length != 3){
+            return false;
+        }
+        hour = Integer.parseInt(endTimeSplit[0]);
+        if(hour>24 || hour<0){
+            return false;
+        }
+        minute = Integer.parseInt(endTimeSplit[1]);
+        if(minute < 0 || minute > 60){
+            return false;
+        }
+        second = Integer.parseInt(endTimeSplit[2]);
+        if(second < 0 || second >60){
+            return false;
+        }
+        return true;
+        
+
+    }
 
 }
